342556016e1f7a30077c714b70355481c77542ff
shubhangi shivdas Deshmukh <68213104+rana348@users.noreply.github.com>
2020-07-22 10:01:53 +0530

    Add files via upload

---
+++ Day2.python.py
@@ -0,0 +1,37 @@
+
+
+
+#Assignment of Day2
+#variable:Syntax:Variable Name=
+value
+
+number=1947
+
+#operators and Assignment Operation and Also formatted Strings
+
+operand1=25
+operand2=5
+operand3=0
+
+#Addition
+Operation=operand1+operand2
+print(f"Addition Value={operation}\n")
+#Escape Sequences
+#Subtaction
+Operation=operand1-operand2
+print(f"Subtarction Value={operation}")
+#Division
+operation=operand1//operand2
+#Return Integer
+print(f"divided value={operation}")
+#Reminder
+operation=operand1%operand2
+print(f'Reminder\'s={operation}')
+#Back Slash
+#Multiplication
+operation=operand1*operand2
+print(f"Multiplied Value={operation}")
+
+#Triple Quotes
+print("""Learning from LetsUpgrade Is Fun""")
+
